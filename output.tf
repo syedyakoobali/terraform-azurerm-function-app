0f533e73416c627dd9f869b1df769cbce223c3d7
Heine Furubotten <heine.furubotten@bouvet.no>
2018-08-06 16:21:20 +0200

    Fixed output which does not support lists with maps in a nice way.

--- output.tf
+++ output.tf
@@ -1,5 +1,9 @@
-output "identity" {
-  description = "The MSI identities set on the function app. Returns a list of identities."
-  value       = "${azurerm_function_app.functionapp.identity}"
+output "identity_principal_id" {
+  description = "The MSI identity principal id set on the function app."
+  value       = "${azurerm_function_app.functionapp.identity.0.principal_id}"
 }
 
+output "identity_tenant_id" {
+  description = "The MSI identity tenant id set on the function app."
+  value       = "${azurerm_function_app.functionapp.identity.0.tenant_id}"
+}
