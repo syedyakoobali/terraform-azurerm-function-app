45532de8011a491c926fbfbdf555f5de90c9dc4c
Joakim Bakke Hellum <joakim.bakke.hellum@innovasjonnorge.no>
2019-05-14 00:06:12 +0200

    Use data source as an early check that resource group exists

--- main.tf
+++ main.tf
@@ -1,10 +1,17 @@
 locals {
   app_service_plan_id = coalesce(var.app_service_plan_id, azurerm_app_service_plan.main[0].id)
+  
   storage_account_name = replace(
     lower(substr(var.name, 0, min(18, length(var.name)))),
     "/[^a-z0-9]/",
     "",
   )
+  
+  location = coalesce(var.location, data.azurerm_resource_group.main.location)
+}
+
+data "azurerm_resource_group" "main" {
+  name = var.resource_group_name
 }
 
 resource "random_id" "main" {
@@ -13,8 +20,8 @@ resource "random_id" "main" {
 
 resource "azurerm_storage_account" "main" {
   name                     = format("%s%s", local.storage_account_name, random_id.main.hex)
-  resource_group_name      = var.resource_group_name
-  location                 = var.location
+  resource_group_name      = data.azurerm_resource_group.main.name
+  location                 = local.location
   account_tier             = "Standard"
   account_replication_type = "RAGRS"
   tags                     = var.tags
@@ -23,8 +30,8 @@ resource "azurerm_storage_account" "main" {
 resource "azurerm_app_service_plan" "main" {
   count               = var.app_service_plan_id == "" ? 1 : 0
   name                = format("%s-plan", var.name)
-  location            = var.location
-  resource_group_name = var.resource_group_name
+  location            = local.location
+  resource_group_name = data.azurerm_resource_group.main.name
 
   sku {
     tier = split("_", var.sku)[0]
@@ -36,8 +43,8 @@ resource "azurerm_app_service_plan" "main" {
 
 resource "azurerm_function_app" "main" {
   name                      = var.name
-  location                  = var.location
-  resource_group_name       = var.resource_group_name
+  location                  = local.location
+  resource_group_name       = data.azurerm_resource_group.main.name
   app_service_plan_id       = local.app_service_plan_id
   storage_connection_string = azurerm_storage_account.main.primary_connection_string
   https_only                = var.https_only
