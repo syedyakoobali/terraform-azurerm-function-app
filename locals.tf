a4d95c29a06fc048e720fd1b37318d5046a4ff71
Niklas Gustafsson <gustafsson.niklas@gmail.com>
2020-04-23 09:48:11 +0200

    Fixed format

--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 locals {
-  storage_account_name    = "${replace("${lower(("${var.function_app_name}funcsta"))}", "/[^a-z0-9]/","")}"
+  storage_account_name    = "${replace("${lower(("${var.function_app_name}funcsta"))}", "/[^a-z0-9]/", "")}"
   app_service_plan_name   = "${var.function_app_name}-plan"
   function_app_name       = "${var.function_app_name}-func"
   autoscale_settings_name = "${var.function_app_name}-autoscale"
